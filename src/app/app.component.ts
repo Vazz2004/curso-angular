c49292d5263701ba5a8c5fb81a47792c6936a210
Vazz2004 <samivazqueles@gmail.com>
2024-06-13 00:08:31 -0500

    🚀 Subo mi primer coomit de curso de angular
    Se vio como inicar un proyecto
    
    Crear coponentes
    
    Como llamar componetes
    
    Manejo de datos desde el ts

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { GamesComponent } from './games/games.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, UserComponent],
+  templateUrl: './app.component.html',
+  styles: `
+
+  `
+})
+
+export class AppComponent {
+  city = 'Bogota'
+}
